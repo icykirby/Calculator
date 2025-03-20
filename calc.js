1e5d59fb1b2b9525fda8f35b0eeec24ba725fd55
icykirby <jamericanmadison@gmail.com>
2025-03-20 15:49:30 -0400

    displayed numbers

--- calc.js
+++ calc.js
@@ -1,17 +1,36 @@
+let answerDisplay = document.getElementById("answer");
+
+function display(input){
+    answerDisplay.value += input;
+}
+
+function displayCalcClear(){
+    
+}
+
+function clear(zero){
+    answerDisplay.value += zero;
+}
+
+
+
+function calc(input){
+
+}
 
 
 function sum(num1, num2){
     return num1 + num2;
-};
+}
 
 function sub(num1, num2){
     return num1 - num2;
-};
+}
 
 function mult(num1, num2){
     return num1 * num2;
-};
+}
 
 function div(num1, num2){
     return num1 / num2;
-};
+}
