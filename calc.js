bb4ae97bcc6e37ec2d3ffe09fab7aa01361e6ab7
icykirby <jamericanmadison@gmail.com>
2025-03-21 04:54:33 -0400

    Added functionality (still errors)

--- calc.js
+++ calc.js
@@ -1,36 +1,193 @@
 let answerDisplay = document.getElementById("answer");
+let answerDisplay2 = document.getElementById("answer2");
+answerDisplay.value = 0;
+answerDisplay2.value = 0;
+let temp = 0;
+operator = '';
+let inputCount = 1;
+let count = 0;
 
-function display(input){
-    answerDisplay.value += input;
+let mathAnswer;
+
+let turn = true;
+let finalAnswer;
+let calcCount = 0;
+
+function clearDisplay(zero){
+    count = 0;
+    answerDisplay.value = 0;
+    answerDisplay2.value = 0;
+    operator = '';
+    turn = true;
+    document.getElementById("clearBtn").innerHTML = 'AC';
 }
 
-function displayCalcClear(){
+function sign(){
+    if(turn = true){
+        mathAnswer = parseFloat(answerDisplay.value, 10);
+        mathAnswer *= -1; 
+        answerDisplay.value = mathAnswer;       
+    }
+    else{
+        mathAnswer2 = parseFloat(answerDisplay2.value, 10);
+        mathAnswer2 *= -1;
+        answerDisplay2.value = mathAnswer2;
+    }
+}
+
+function percent(){
+    if(turn == true){
+        mathAnswer = parseFloat(answerDisplay.value, 10);
+        mathAnswer /= 100; 
+        answerDisplay.value = mathAnswer;       
+    }
+    else{
+        mathAnswer2 = parseFloat(answerDisplay2.value, 10);
+        mathAnswer2 /= 100;
+        answerDisplay2.value = mathAnswer2;
+    }
+}
+
+let decCount = 0;
+function decimal(dec){
+    console.log(decCount);
+    console.log(turn);
+    if(decCount == 0 && turn == true){
+        answerDisplay.value += dec;
+        decCount++;
+        console.log("hi");
+    }
+    if(decCount == 0 && turn == false){
+        answerDisplay2.value += dec;
+        decCount++;
+    }
+}
+
+function display(finAns){
+    console.log("calcCount: " + calcCount);
+    console.log(mathAnswer);
+    console.log(mathAnswer2);
+    answerDisplay.value = null;
+    answerDisplay.value += finAns;
+    turn = false;
+}
+
+
+function getNum(input){
+    
+    console.log(turn);
+
+    if(answerDisplay.value == 0 ){
+        answerDisplay.value = null;
+    }
+    if(answerDisplay2.value == 0){
+        answerDisplay2.value = null;
+    }
+    document.getElementById("clearBtn").innerHTML = 'C';
+  
+        
+    if(turn == true && inputCount <= 9){
+        answerDisplay.value += input;
+        console.log("logged");
+    }    
+    if(turn == false && inputCount <= 9){
+        
+        answerDisplay.value = null;
+        answerDisplay.value += input;
+        console.log("logged");
+    }
+    inputCount++;
+    
+    console.log(turn);
     
 }
 
-function clear(zero){
-    answerDisplay.value += zero;
+
+
+function calc(){
+    if(calcCount > 0){
+        
+        finalAnswer += mathAnswer2;
+    }
+    mathAnswer = temp;
+    console.log(mathAnswer);
+    mathAnswer2 = parseFloat(answerDisplay.value, 10);
+    console.log(mathAnswer2);
+
+    switch(operator){
+        case '+':
+            finalAnswer = mathAnswer + mathAnswer2;
+        break;
+        case '-':
+            finalAnswer = mathAnswer - mathAnswer2;
+        break;
+        case '*':
+            finalAnswer = mathAnswer * mathAnswer2;
+        break;
+        case '/':
+            finalAnswer = mathAnswer / mathAnswer2;
+        break;
+    }
+
+    console.log(finalAnswer);
+    answerDisplay2.value = null;
+    operator = '';
+    display(finalAnswer);
+    calcCount++;
+    decCount = 0;
+    
+}
+
+function sum(){
+    console.log(count);
+    if(count > 0){
+        
+        console.log("calc >  0");
+        calc();
+    }
+    temp = parseFloat(answerDisplay.value, 10);
+    turn = false;
+    operator += '+';
+    count++;
+}
+
+function subt(){
+    console.log(count);
+    if(count > 0){
+        
+        console.log("calc >  0");
+        calc();
+    }
+    temp = parseFloat(answerDisplay.value, 10);
+    turn = false;
+    operator += '-';
+    count++;
+}
+
+function mult(){
+    console.log(count);
+    if(count > 0){
+        
+        console.log("calc >  0");
+        calc();
+    }
+    temp = parseFloat(answerDisplay.value, 10);
+    turn = false;
+    operator += '*';
+    count++;
 }
 
 
-
-function calc(input){
-
+function div(){
+    console.log(count);
+    if(count > 0){
+        
+        console.log("calc >  0");
+        calc();
+    }
+    temp = parseFloat(answerDisplay.value, 10);
+    turn = false;
+    operator += '/';
+    count++;
 }
 
-
-function sum(num1, num2){
-    return num1 + num2;
-}
-
-function sub(num1, num2){
-    return num1 - num2;
-}
-
-function mult(num1, num2){
-    return num1 * num2;
-}
-
-function div(num1, num2){
-    return num1 / num2;
-}
