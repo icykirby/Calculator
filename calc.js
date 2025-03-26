ae078378ae567a964513b65ebeee58caa07fa907
icykirby <jamericanmadison@gmail.com>
2025-03-26 02:16:10 -0400

    Debugging exponents and signs

--- calc.js
+++ calc.js
@@ -4,14 +4,25 @@ answerDisplay.value = 0;
 answerDisplay2.value = 0;
 let temp = 0;
 operator = '';
-let inputCount = 1;
+let inputCount = 0;
 let count = 0;
-
+let nullCount = 0;
 let mathAnswer;
-
+let numString = null;
 let turn = true;
 let finalAnswer;
 let calcCount = 0;
+let finAnsLenStr = null;
+let finAnsLen = 0;
+let fontSizeNum = 100;
+
+
+function changeFontsize(){
+    if(inputCount > 4){
+        fontSizeNum -= 10;
+        answerDisplay.style.fontSize = fontSizeNum + "px";
+    }
+}
 
 function clearDisplay(zero){
     count = 0;
@@ -20,18 +31,39 @@ function clearDisplay(zero){
     operator = '';
     turn = true;
     document.getElementById("clearBtn").innerHTML = 'AC';
+    inputCount = 0;
+    decCount = 0;
+    fontSizeNum = 100;
+    answerDisplay.style.fontSize = fontSizeNum + "px";
 }
 
 function sign(){
     if(turn = true){
         mathAnswer = parseFloat(answerDisplay.value, 10);
         mathAnswer *= -1; 
-        answerDisplay.value = mathAnswer;       
+        let hasE = answerDisplay.value;
+        if(hasE.includes('e')){
+            if(hasE.startsWith('-')){
+                answerDisplay.value = hasE.slice(1);
+            }
+            else{
+                answerDisplay.value = '-' + hasE;
+            }
+        }      
     }
     else{
         mathAnswer2 = parseFloat(answerDisplay2.value, 10);
         mathAnswer2 *= -1;
-        answerDisplay2.value = mathAnswer2;
+        let hasE = answerDisplay.value;
+        if(hasE.includes('e')){
+            if(hasE.startsWith('-')){
+                answerDisplay.value = hasE.slice(1);
+            }
+            else{
+                answerDisplay.value = '-' + hasE;
+            }
+        }
+        answerDisplay.value = mathAnswer2;
     }
 }
 
@@ -42,9 +74,9 @@ function percent(){
         answerDisplay.value = mathAnswer;       
     }
     else{
-        mathAnswer2 = parseFloat(answerDisplay2.value, 10);
+        mathAnswer2 = parseFloat(answerDisplay.value, 10);
         mathAnswer2 /= 100;
-        answerDisplay2.value = mathAnswer2;
+        answerDisplay.value = mathAnswer2;
     }
 }
 
@@ -52,13 +84,13 @@ let decCount = 0;
 function decimal(dec){
     console.log(decCount);
     console.log(turn);
-    if(decCount == 0 && turn == true){
+    if(decCount == 0 && turn == true && inputCount < 9){
         answerDisplay.value += dec;
         
         console.log("hi");
         decCount++;
     }
-    if(decCount == 0 && turn == false){
+    if(decCount == 0 && turn == false && inputCount < 9){
         answerDisplay.value += dec;
         
         console.log("hi2");
@@ -70,8 +102,42 @@ function display(finAns){
     console.log("calcCount: " + calcCount);
     console.log(mathAnswer);
     console.log(mathAnswer2);
-    answerDisplay.value = null;
-    answerDisplay.value += finAns;
+    console.log(finAns);
+
+    if(finAns >= 1000000000){
+        //1.52416 e16 = 15241 578750190520
+        //1,000,000,000 = 1e9
+        numString = finAns.toString();
+        finAnsLenStr = numString.length;
+        finAnsLen = parseInt(finAnsLenStr, 10);
+        finAnsLen -= 1;
+        let w = finAns / 10 ** finAnsLen;
+        let y = +w.toFixed(5);
+        
+    
+        answerDisplay.value = null;
+        answerDisplay.value += y + "e" + finAnsLen;
+        
+        
+        let x = finAns.toExponential();
+        console.log("w: " + w);
+        console.log("x: " + x);
+        console.log("y: " + y);
+
+
+    }
+    else{
+        numString = finAns.toString();
+        finAnsLenStr = numString.length;
+        finAnsLen = parseInt(finAnsLenStr, 10);
+        finAnsLen -= 1;
+        console.log(finAnsLen);
+        
+        v = +finAns.toPrecision(2);
+        answerDisplay.value = null;
+        answerDisplay.value += v;
+    }
+    
     turn = false;
 }
 
@@ -89,32 +155,32 @@ function getNum(input){
     document.getElementById("clearBtn").innerHTML = 'C';
   
         
-    if(turn == true && inputCount <= 9){
+    if(turn == true && inputCount < 9){
+        changeFontsize();
         answerDisplay.value += input;
-        console.log("logged");
+        console.log("logged 1");
     }    
 
-
-
-    if(turn == false && inputCount <= 9){
-        
-        
+    if(turn == false && inputCount < 9){
+        if(nullCount == 0){
+            answerDisplay.value = null;
+            nullCount++;
+        }
+        changeFontsize();
         answerDisplay.value += input;
-        console.log("logged");
+        console.log("logged 2");
     }
     inputCount++;
-    
+    console.log("inputCount: " + inputCount);
     console.log(turn);
     
 }
 
 
 
+
 function calc(){
-    if(calcCount > 0){
-        
-        finalAnswer += mathAnswer2;
-    }
+    
     mathAnswer = temp;
     console.log(mathAnswer);
     mathAnswer2 = parseFloat(answerDisplay.value, 10);
@@ -141,6 +207,8 @@ function calc(){
     display(finalAnswer);
     calcCount++;
     decCount = 0;
+    nullCount = 0;
+    inputCount = 0;
     
 }
 
@@ -171,6 +239,7 @@ function subt(){
     turn = false;
     operator += '-';
     count++;
+    inputCount = 0;
 }
 
 function mult(){
@@ -185,6 +254,7 @@ function mult(){
     turn = false;
     operator += '*';
     count++;
+    inputCount = 0;
 }
 
 
@@ -200,5 +270,6 @@ function div(){
     turn = false;
     operator += '/';
     count++;
+    inputCount = 0;
 }
 
